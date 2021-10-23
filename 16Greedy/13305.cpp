98af5d3b31fe7fc0700906f080c8328c7d9e5d2d
YBI <quddlr96@naver.com>
2021-10-23 17:31:14 +0900

    ~10/20

--- 16Greedy/13305.cpp
+++ 16Greedy/13305.cpp
@@ -5,22 +5,16 @@ using namespace std;
 int main(){
     long n;
     cin >> n;
-    if(n<2||n>1000) return 0;
     vector<long> dist;
     vector<long> cost;
-    long totaldist =0;
     for(int i=0;i<n-1;i++){
         long x;
         cin >> x;
-        totaldist += x;
         dist.push_back(x);
     }
-    if(totaldist>10000) return 0;
-
     for(int i=0;i<n;i++){
         long x;
         cin >>x;
-        if(x>10000) return 0;
         cost.push_back(x);
     }
     long totalcost =0;
