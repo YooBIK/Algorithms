d1c540aa91d45efce2bde6acbf137f6c21bbced6
Yoobi <quddlr96@naver.com>
2022-03-03 00:01:24 +0900

    ~0303

--- sw/dfs_bfs/1987.cpp
+++ sw/dfs_bfs/1987.cpp
@@ -1,50 +1,47 @@
-#include<iostream>
+#include <iostream>
 #include <queue>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
+
 string map[20];
-int visit[20][20] = {0,};
 
 int dir_x[4] = {1,0,-1,0};
 int dir_y[4] = {0,1,0,-1};
 
 int r,c;
 
-vector<char> vec;
-int count =0;
+int alpha[26] = {0,};
+int result = -1;
 
 
-void dfs(int x,int y){
-    visit[x][y] =1;
+void dfs(int x,int y,int cnt){
+    result = max(cnt,result);
+
     for(int i=0;i<4;i++){
         int nx = x + dir_x[i];
         int ny = y + dir_y[i];
-        int flag = 0;
         
-        if(nx>=0&&nx<20&&ny>=0&&ny<20&&visit[nx][ny]==0){
-            for(int j=0;j<vec.size();j++){
-                if(map[nx][ny]==vec[j]){
-                    flag=1;
-                }
-            }
-            if(flag==0){
-                vec.push_back(map[nx][ny]);
-                visit[nx][ny]=1;
-                count++;
-                dfs(nx,ny);
+        if(nx>=0&&nx<r&&ny>=0&&ny<c){
+            if(alpha[map[nx][ny]-'A']==0){
+                alpha[map[nx][ny]-'A'] = 1;
+                dfs(nx,ny,cnt+1);
+                alpha[map[nx][ny]-'A']=0;
             }
         }
     }
 }
+
 
 int main(){
     cin >> r>>c;
     for(int i=0;i<r;i++){
         cin >> map[i];
     }
-    vec.push_back(map[0][0]);
-    dfs(0,0);
-    cout << count;
+
+    alpha[map[0][0]-'A'] = 1;
+    dfs(0,0,1);
+    cout << result;
     return 0;
 }
